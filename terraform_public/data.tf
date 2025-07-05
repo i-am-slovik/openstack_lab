84004c8de043f10052b692a3f604c66defa2f0c1
i-am-slovik <slowikmuzyka@gmail.com>
2025-07-05 20:20:45 +0200

    instances

--- terraform_public/data.tf
+++
@@ -1,9 +0,0 @@
-#data
-
-data "openstack_networking_port_v2" "slovik_dmz_ports" {
-  for_each = {
-    slovik_ingress_node = "slovik_ingress_node"
-    slovik_jumpbox      = "slovik_jumpbox"
-  }
-  name = each.value
-}
--- terraform_public/ports.tf
+++ terraform_public/ports.tf
@@ -1,80 +1,69 @@
 # ports
 
+locals {
+  slovik_dmz_ports_map = {
+    "slovik_ingress_node_port" = var.dmz_ips.ingress_node
+    "slovik_jumpbox_port"      = var.dmz_ips.jumpbox
+  }
+
+  slovik_internal_management_ports_map = {
+    "slovik_internal_https_ingress_node_port" = var.internal_ips.internal_https_ingress_node
+    "slovik_internal_jumpbox_port"            = var.internal_ips.internal_jumpbox
+    "slovik_zabbix_port"                      = var.internal_ips.zabbix
+    "slovik_internal_management_13_port"      = var.internal_ips.internal_management_13
+    "slovik_internal_management_14_port"      = var.internal_ips.internal_management_14
+    "slovik_internal_management_15_port"      = var.internal_ips.internal_management_15
+    "slovik_internal_management_16_port"      = var.internal_ips.internal_management_16
+    "slovik_internal_management_17_port"      = var.internal_ips.internal_management_17
+    "slovik_internal_management_18_port"      = var.internal_ips.internal_management_18
+    "slovik_internal_management_19_port"      = var.internal_ips.internal_management_19
+    "slovik_internal_management_20_port"      = var.internal_ips.internal_management_20
+  }
+
+  slovik_internal_https_ports_map = {
+    "slovik_internal_https_ingress_node_port" = var.internal_ips.internal_https_ingress_node
+    "slovik_internal_https_11_port"           = var.internal_ips.internal_https_11
+    "slovik_internal_https_12_port"           = var.internal_ips.internal_https_12
+    "slovik_internal_https_13_port"           = var.internal_ips.internal_https_13
+    "slovik_internal_https_14_port"           = var.internal_ips.internal_https_14
+    "slovik_internal_https_15_port"           = var.internal_ips.internal_https_15
+    "slovik_internal_https_16_port"           = var.internal_ips.internal_https_16
+    "slovik_internal_https_17_port"           = var.internal_ips.internal_https_17
+    "slovik_internal_https_18_port"           = var.internal_ips.internal_https_18
+    "slovik_internal_https_19_port"           = var.internal_ips.internal_https_19
+    "slovik_internal_https_20_port"           = var.internal_ips.internal_https_20
+  }
+}
+
 resource "openstack_networking_port_v2" "slovik_dmz_ports" {
-  count      = 2
-  name       = element(["slovik_ingress_node", "slovik_jumpbox"], count.index)
+  for_each  = local.slovik_dmz_ports_map
+  name      = each.key
   network_id = openstack_networking_network_v2.slovik_dmz_network.id
+
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.slovik_dmz_subnet.id
-    ip_address = element([var.dmz_ips.ingress_node, var.dmz_ips.jumpbox], count.index)
+    ip_address = each.value
   }
 }
 
 resource "openstack_networking_port_v2" "slovik_internal_management_ports" {
-  count      = 11
-  name       = element([
-    "slovik_internal_https_ingress_node",
-    "slovik_internal_jumpbox",
-    "slovik_zabbix",
-    "slovik_internal_management_13",
-    "slovik_internal_management_14",
-    "slovik_internal_management_15",
-    "slovik_internal_management_16",
-    "slovik_internal_management_17",
-    "slovik_internal_management_18",
-    "slovik_internal_management_19",
-    "slovik_internal_management_20"
-  ], count.index)
+  for_each  = local.slovik_internal_management_ports_map
+  name      = each.key
   network_id = openstack_networking_network_v2.slovik_internal_management.id
+
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.slovik_internal_management_subnet.id
-    ip_address = element([
-      var.internal_ips.internal_https_ingress_node,
-      var.internal_ips.internal_jumpbox,
-      var.internal_ips.zabbix,
-      var.internal_ips.internal_management_13,
-      var.internal_ips.internal_management_14,
-      var.internal_ips.internal_management_15,
-      var.internal_ips.internal_management_16,
-      var.internal_ips.internal_management_17,
-      var.internal_ips.internal_management_18,
-      var.internal_ips.internal_management_19,
-      var.internal_ips.internal_management_20
-    ], count.index)
+    ip_address = each.value
   }
 }
 
 resource "openstack_networking_port_v2" "slovik_internal_https_ports" {
-  count      = 11
-  name       = element([
-    "slovik_internal_https_ingress_node",
-    "slovik_internal_https_11",
-    "slovik_internal_https_12",
-    "slovik_internal_https_13",
-    "slovik_internal_https_14",
-    "slovik_internal_https_15",
-    "slovik_internal_https_16",
-    "slovik_internal_https_17",
-    "slovik_internal_https_18",
-    "slovik_internal_https_19",
-    "slovik_internal_https_20"
-  ], count.index)
+  for_each  = local.slovik_internal_https_ports_map
+  name      = each.key
   network_id = openstack_networking_network_v2.slovik_internal_https.id
+
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.slovik_internal_https_subnet.id
-    ip_address = element([
-      var.internal_ips.internal_https_ingress_node,
-      var.internal_ips.internal_https_11,
-      var.internal_ips.internal_https_12,
-      var.internal_ips.internal_https_13,
-      var.internal_ips.internal_https_14,
-      var.internal_ips.internal_https_15,
-      var.internal_ips.internal_https_16,
-      var.internal_ips.internal_https_17,
-      var.internal_ips.internal_https_18,
-      var.internal_ips.internal_https_19,
-      var.internal_ips.internal_https_20
-    ], count.index)
+    ip_address = each.value
   }
 }
-
